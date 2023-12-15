8887100498922a059b30570e5cc501b5d677051b
AntonioTarasenko <toxata1@gmail.com>
2023-12-15 13:08:16 +0200

    ышо

--- js/script.js
+++ js/script.js
@@ -82,6 +82,15 @@ const aAbout = document.getElementById('a-about');
 const aEducation = document.getElementById('a-education');
 const aPortfolio = document.getElementById('a-portfolio');
 const aContact = document.getElementById('a-contact');
+const aboutMe = document.getElementById('aboutMe');
+const aboutH3 = document.getElementById('aboutH3');
+const aboutP1 = document.getElementById('aboutP1');
+const aboutP2 = document.getElementById('aboutP2');
+const aboutP3 = document.getElementById('aboutP3');
+const aboutP4 = document.getElementById('aboutP4');
+const aboutP5 = document.getElementById('aboutP5');
+const aboutP6 = document.getElementById('aboutP6');
+const aboutPend = document.getElementById('aboutPend');
 
 let isRussian = true;
 
@@ -101,6 +110,17 @@ let isRussian = true;
         aEducation.textContent = "Activities";
         aPortfolio.textContent = "Portfolio";
         aContact.textContent = "Contacts";
+
+        aboutMe.innerHTML = "About <span>Me</span>";
+        aboutP1.textContent= "Samira, the most stylish and vibrant star in this world, where every moment of her life becomes a unique work of art. Her love for fashion is not just a passion; it's a breath, breathed into every created outfit, into every expressive look.";
+        aboutP2.textContent= "In her eyes, dance is not just movement; it's a way to transform her love for life and passion for creativity into music. Every movement of her body is the harmony of dance, a unique rhythm that she creates, feeling the freedom of self-expression.";
+        aboutP3.textContent= "Her wardrobe is a true palette, where each color reflects her mood, and each fabric tells its own story. She orchestrates a real fashion show on the catwalks of life, where every step is a dance, every stride an expression of confidence and elegance.";
+        aboutP4.textContent= "Samira, that name sounds like a melody, and in every movement of hers, the harmony of dance is encapsulated, creating a unique rhythm of her life. Loving to unite fashion and dance, she embodies her ideas in harmonious compositions of styles and movements.";
+        aboutP5.textContent= "Her passion for fashion is not just an interest; it's a source of inspiration, an inner fire ignited by the sounds of her favorite music. When she dances, her movements unfold the pages of a book, where every trick is the next crucial moment in her amazing story.";
+        aboutP6.textContent= `"My outfits are my language, and dances are my way of communicating with the world. Together, they create a unique kaleidoscope of my creative soul." Samira doesn't just dance in the world of fashion; she creates her own, unparalleled choreographic pattern of life.`;
+        aboutPend.innerHTML= `Age: 9 years (11/28/2014) <br>
+        Height: 146 cm <br>Shoe size 37<br>
+        Brown eyes, brunette`;
       } else {
         mainLogo.textContent = "Барсегян Самира";
         homeH3.textContent = "Модель, Дети";
@@ -114,5 +134,17 @@ let isRussian = true;
         aEducation.textContent = "Деятельность";
         aPortfolio.textContent = "Портфолио";
         aContact.textContent = "Контакты";
+
+        aboutMe.innerHTML = "Обо <span>мне</span>";
+        aboutH3.textContent= "Модель, Дети!";
+        aboutP1.textContent= "Самира, самая стильная и яркая звезда в этом мире, где каждый момент её жизни превращается в уникальное произведение искусства. Её любовь к моде — это не просто страсть, это дыхание, вдыхаемое в каждый созданный наряд, в каждый выразительный образ.";
+        aboutP2.textContent= "В её глазах танец — не просто движение, а способ превратить свою любовь к жизни и страсть к творчеству в музыку. Каждое движение её тела — это гармония танца, уникальный ритм, который она создаёт, ощущая свободу самовыражения.";
+        aboutP3.textContent= "Её гардероб — это настоящая палитра, на которой каждый цвет — отражение её настроения, каждая ткань — собственная история. Она устраивает настоящий показ стиля на модных подиумах жизни, где каждый шаг — это танец, каждая походка — выражение уверенности и элегантности.";
+        aboutP4.textContent= "Самира, это имя, звучит как мелодия, а в каждом её движении заключена гармония танца, создавая уникальный ритм своей жизни. Любя объединять моду и танец, она воплощает свои идеи в гармоничных композициях стилей и движений.";
+        aboutP5.textContent= "Её страсть к моде не просто интерес, а источник вдохновения, внутренний огонь, разгорающийся под звуки любимой музыки. Когда она танцует, её движения словно раскрывают страницы книги, где каждый трюк — следующий главный момент в её удивительной истории.";
+        aboutP6.textContent= "Мои наряды — это мой язык, а танцы — мой способ общения с миром. Вместе они создают уникальный калейдоскоп моей творческой души. Самира не просто танцует в мире моды; она создаёт свой собственный, неповторимый хореографический рисунок жизни.";
+        aboutPend.innerHTML= `Возраст: 9 лет (28.11.2014) <br>
+        Рост: 146 см<br>Размер обуви 37<br>
+        Глаза карие, брюнетка`;
       }
     });
